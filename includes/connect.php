d862dc92bcafba592805bc80b1d95aa9ceab05a4
TijnKlabbers <1195159@student.roc-nijmegen.nl>
2022-03-31 08:52:55 +0200

    nieuwe code

--- includes/connect.php
+++ includes/connect.php
@@ -6,22 +6,14 @@ $pass = '';
 $charset = 'utf8mb4';
 
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$opt = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXEPTION,
-    PDO:: ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTER_EMULATE_PREPARES => false,
-];
 
-try
-{
-    $connect = new PDO($dsn, $user, $pass, $opt);
-    echo "Verbinding is gemaakt.";
-
-}
-catch (PDOExeption $e)
-{
-    echo $e->getMessage();
-    die("sorry, Database probleem");
-}
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+?>
 
